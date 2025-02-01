13aa176354ba381f428e02963fabed8492104051
moonyango <mosesadrian825@gmail.com>
2025-02-01 16:20:48 +0300

    feat: init tables and handlers and routes. set up a listen and serve

---
+++ backend/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"real-time-forum/backend/database"
+	"real-time-forum/backend/handlers"
+	"real-time-forum/backend/middleware"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type App struct {
+	Users *database.UserModel
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "./forum.db")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	app := App{
+		Users: &database.UserModel{
+			DB: db,
+		},
+	}
+	app.Users.InitTables()
+
+	h := handlers.NewHandler(app.Users)
+
+	mwApp := &middleware.App{
+		Users:    app.Users,
+		Handlers: h,
+	}
+	router := mwApp.RouteChecker(mwApp.Routes())
+
+	log.Println("Server started on :8000")
+
+	if err := http.ListenAndServe(":8000", router); err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
+}
