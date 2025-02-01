eb25b0ac9021919e882b60e6e4a52b8ce7b0adcd
moonyango <mosesadrian825@gmail.com>
2025-02-01 12:37:14 +0300

    fix: wrap the gender select into a div

--- frontend/js/templates.js
+++ frontend/js/templates.js
@@ -1,6 +1,6 @@
 export const loginTemplate = () => `
    <div class="login-form-container">
-    <h1>Login Form</h1>
+    <h1>Login</h1>
     <input type="text" placeholder="Username or Email" class="input-field" />
     <br /><br />
     <input type="password" placeholder="Password" class="input-field" />
@@ -11,32 +11,29 @@ export const loginTemplate = () => `
 
 export const signupTemplate = () => `
     <div class="signup-form-container">
-      <h1>Sign Up Form</h1>
+      <h1>Sign Up</h1>
       <input type="text" placeholder="First Name" class="input-field" />
+      <br /><br />
       <input type="text" placeholder="Second Name" class="input-field" />
       <br /><br />
       <input type="text" placeholder="Username" class="input-field" />
-        <br /><br />
-        <input type="email" placeholder="Email" class="input-field" />
-        <br /><br />
-        <select name="Gender" id="Gender">
-       <option value="male">Male</option>
-       <option value="female">Female</option>
-     </select>
+      <br /><br />
+      <input type="email" placeholder="Email" class="input-field" />
+      <br /><br />
+      <div class="gender-container">
+        <label for="Gender" class="gender-label">Gender:</label>
+        <select name="Gender" id="Gender" class="gender">
+         <option value="male">Male</option>
+         <option value="female">Female</option>
+        </select>
+      </div>
      <br /><br />
     <!-- Age Input -->
-    <div class="age-selector">
-       <label for="age">Age:</label>
        <input type="number" id="age" class="input-field" placeholder="Enter age" />
-       <br />
-       <div class="age-buttons">
-           <button type="button" onclick="decrementAge()">-</button>
-           <span id="ageValue">18</span> <!-- Initial age set to 18 -->
-           <button type="button" onclick="incrementAge()">+</button>
-       </div>
-   </div>
+  
    <br /><br />
    <input type="password" placeholder="Password" class="input-field" />
+   <br /><br />
    <input type="password" placeholder="Confirm Password" class="input-field" />
    <br /><br />
    <button class="signup-button" type="button">Sign Up</button>
