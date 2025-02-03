c401c35a26e5559e9c3b6d263b2c18214363a3e2
moonyango <mosesadrian825@gmail.com>
2025-02-03 19:23:17 +0300

    fix: modify users table

--- backend/database/tables.go
+++ backend/database/tables.go
@@ -12,14 +12,12 @@ type UserModel struct {
 const USERS_TABLE string = `CREATE TABLE IF NOT EXISTS USERS (
     user_id VARCHAR(255) PRIMARY KEY NOT NULL UNIQUE,
     first_name VARCHAR(50) NOT NULL,
-    second_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     username VARCHAR(20) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     gender VARCHAR(10) NOT NULL,
     age INT NOT NULL,
     password VARCHAR(255) NOT NULL,
-    avatar_url VARCHAR(255),
-    auth_provider VARCHAR(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );`
 
