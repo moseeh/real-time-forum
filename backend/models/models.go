5a0352c3c940ca9729a33ce287e808c4b61c0b75
moonyango <mosesadrian825@gmail.com>
2025-02-04 18:40:28 +0300

    feat: user struct

--- backend/models/models.go
+++ backend/models/models.go
@@ -1,1 +1,12 @@
 package models
+
+type User struct {
+	ID           string    `json:"id"`
+	FirstName    string `json:"firstName"`
+	LastName     string `json:"lastName"`
+	Username     string `json:"username"`
+	Email        string `json:"email"`
+	Gender       string `json:"gender"`
+	Age          int    `json:"age"`
+	PasswordHash string `json:"-"`
+}
