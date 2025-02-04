3625719e2a3c2dde561899988b06bf1a595875e5
moonyango <mosesadrian825@gmail.com>
2025-02-04 18:39:16 +0300

    feat: function to get user by username or email

--- backend/database/user_queries.go
+++ backend/database/user_queries.go
@@ -1,6 +1,14 @@
 package database
 
-import "fmt"
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"real-time-forum/backend/models"
+)
+
+var ErrNoRecord = errors.New("no matching record found")
 
 func (m *UserModel) InsertUser(id, first_name, last_name, username, email, gender, password string, age int) error {
 	const USER_INSERT string = "INSERT INTO USERS (user_id, first_name, last_name,  username, email, gender, age, password) VALUES (?,?,?,?,?,?,?,?);"
@@ -34,4 +42,35 @@ func (m *UserModel) UserExists(email, username string) (bool, error) {
 		return false, err
 	}
 	return count > 0, nil
+}
+
+func (m *UserModel) GetUserByEmailOrUsername(identifier string) (*models.User, error) {
+	const GET_USER string = `SELECT user_id, first_name, last_name, username, email, gender, age, password 
+	     FROM USERS where email = ? OR username = ?;`
+
+	stmt, err := m.DB.Prepare(GET_USER)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	user := &models.User{}
+
+	err = stmt.QueryRow(identifier, identifier).Scan(
+		&user.ID,
+		&user.FirstName,
+		&user.LastName,
+		&user.Username,
+		&user.Email,
+		&user.Gender,
+		&user.Age,
+		&user.PasswordHash,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, ErrNoRecord
+		}
+		return nil, err
+	}
+	return user, nil
 }
