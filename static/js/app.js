b10ff223645d3bed76bee5cb41285f70a832559a
aosindo <andyovvo8@gmail.com>
2025-02-05 15:34:53 +0300

    chore: added confirm name

--- static/js/app.js
+++ static/js/app.js
@@ -10,6 +10,42 @@ export function signup() {
   render(signupTemplate); // Render the signup template
   document.querySelector(".button-1").style.cssText = "display: none";
   document.querySelector(".button-2").style.cssText = "display: block";
+  const  username = document.getElementById("signup-username")
+  const  email = document.getElementById("signup-email")
+
+  username.addEventListener("input", debounce(confirmName, 300));
+  // email.addEventListener("input", debounce(confirmEmail, 300));
+
+  async function confirmName() {
+    const user = username.value
+    console.log(user)
+    try {
+      const response = await fetch('/check-username', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username: user }),
+      });
+
+      // Check if the response is OK (status code 200-299)
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      // Decode the JSON response
+      const data = await response.json();
+      console.log(data);
+      // Handle the response
+      if (data.success) {
+          console.log("Username is available!");
+      } else {
+          console.log("Error:", data.message);
+      }
+  } catch (error) {
+      console.error("Failed to fetch:", error);
+  }
+  }
 
   const signupButton = document.getElementById("signup-form-button");
   if (signupButton) {
@@ -35,3 +71,11 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("login-button").addEventListener("click", login);
   login();
 });
+
+function debounce(func, delay) {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(this, args), delay);
+  };
+}
