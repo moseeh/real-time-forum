4b75264813e98305f720296db3bce4c0997866a6
moonyango <mosesadrian825@gmail.com>
2025-02-05 18:31:37 +0300

    feat: new tables CONTENTS TABLE AND USER INTERACTIONS TABLE

--- backend/database/tables.go
+++ backend/database/tables.go
@@ -21,7 +21,7 @@ const USERS_TABLE string = `CREATE TABLE IF NOT EXISTS USERS (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );`
 
-const TOKENS = `
+const SESSIONS_TABLE string = `
     CREATE TABLE IF NOT EXISTS SESSIONS(
         session_id VARCHAR(255) PRIMARY KEY NOT NULL UNIQUE,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -31,12 +31,39 @@ const TOKENS = `
     );
 `
 
-var Tables = []string{USERS_TABLE, TOKENS}
+const CONTENTS_TABLE string = ` CREATE TABLE IF NOT EXISTS CONTENTS (
+	content_id VARCHAR(255) PRIMARY KEY NOT NULL UNIQUE,
+	user_id VARCHAR(255) NOT NULL,
+	parent_id VARCHAR(255),
+	content_type VARCHAR(10) NOT NULL CHECK (content_type IN ('post', 'comment', 'subcomment')),
+	text TEXT NOT NULL,
+	likes_count INTEGER DEFAULT 0,
+	dislikes_count INTEGER DEFAULT 0, 
+	comment_count INTEGER DEFAULT 0,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (user_id) REFERENCES USERS(user_id),
+	FOREIGN KEY (parent_id) REFERENCES CONTENTS(content_id) 
+);
+`
+
+const USER_INTERACTIONS_TABLE string = `CREATE TABLE IF NOT EXISTS USER_INTERACTIONS(
+	interaction_id VARCHAR(255) PRIMARY KEY NOT NULL UNIQUE,
+	user_id VARCHAR(255) NOT NULL,
+	content_id VARCHAR(255) NOT NULL,
+	interaction_type VARCHAR(10) NOT NULL CHECK (interaction_type IN ('like', 'dislike')),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (user_id) REFERENCES USERS(user_id),
+	FOREIGN KEY (content_id) REFERENCES CONTENTS(content_id),
+	UNIQUE(user_id, content_id, interaction_type)
+);`
+
+var Tables = []string{USERS_TABLE, SESSIONS_TABLE, CONTENTS_TABLE, USER_INTERACTIONS_TABLE}
 
 func (m *UserModel) InitTables() {
 	for _, statement := range Tables {
 		if err := m.executeStatement(statement); err != nil {
-			log.Printf("Error creating table: %v", err)
+			log.Printf("Error creating table: %v%v", err, statement)
 		}
 	}
 }
