71dff97c701b7cb15b4c7ab5fca6a4c9d53f2cc5
moonyango <mosesadrian825@gmail.com>
2025-02-06 18:54:50 +0300

    feat: function to remove user_interaction to table

--- backend/database/user_interaction_queries.go
+++ backend/database/user_interaction_queries.go
@@ -1,10 +1,18 @@
 package database
 
-import "real-time-forum/backend/models"
+import (
+	"real-time-forum/backend/models"
+)
 
 func (m *UserModel) AddUserInteraction(interaction *models.UserInteraction) error {
 	query := `INSERT INTO USER_INTERACTIONS (interaction_id, user_id, content_id, interaction_type) VALUES (?, ?, ?, ?)`
 
 	_, err := m.DB.Exec(query, interaction.InteractionID, interaction.UserID, interaction.ContentID, interaction.InteractionType)
+	return err
+}
+
+func (m *UserModel) RemoveUserInteraction(userID, contentID string) error {
+	query := `DELETE FROM USER_INTERACTIONS WHERE user_id = ? AND content_id = ?`
+	_, err := m.DB.Exec(query, userID, contentID)
 	return err
 }
