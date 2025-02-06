8987746c185250fc2b4c38e49887f88bdae3b186
moonyango <mosesadrian825@gmail.com>
2025-02-06 18:37:47 +0300

    feat: function to insert a content into a db

---
+++ backend/database/content_queries.go
@@ -0,0 +1,13 @@
+package database
+
+import (
+	"database/sql"
+
+	"real-time-forum/backend/models"
+)
+
+func (m *UserModel) InsertContent(tx *sql.Tx, content *models.Content) error {
+	query := `INSERT INTO CONTENTS (content_id, user_id, parent_id, content_type, title, text) VALUES (?, ?, ?, ?, ?, ?)`
+	_, err := tx.Exec(query, content.ID, content.AuthorID, content.ParentID, content.ContentType, content.Title, content.Text)
+	return err
+}
