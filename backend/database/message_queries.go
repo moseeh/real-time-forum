31221e1d92d53295c7ac92acbc9b6b8eeb2da535
aosindo <andyovvo8@gmail.com>
2025-02-12 12:22:31 +0300

    fix: insert message id to database

--- backend/database/message_queries.go
+++ backend/database/message_queries.go
@@ -2,8 +2,8 @@ package database
 
 import "database/sql"
 
-func (m *UserModel) SaveMessage(tx *sql.Tx, senderID, receiverID, message string) error {
-	query := `INSERT INTO messages (senderId, receiverId, message) VALUES (?, ?, ?)`
-	_, err := tx.Exec(query, senderID, receiverID, message )
+func (m *UserModel) SaveMessage(tx *sql.Tx, messageID, senderID, receiverID, message string) error {
+	query := `INSERT INTO messages (message_id, senderId, receiverId, message) VALUES (?, ?, ?, ?)`
+	_, err := tx.Exec(query, messageID, senderID, receiverID, message)
 	return err
 }
