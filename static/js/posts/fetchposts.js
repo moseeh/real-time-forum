30cae1a2523b90f6cefc7edc479e5239580c61ce
moonyango <mosesadrian825@gmail.com>
2025-02-19 15:26:44 +0300

    fix: fetching posts

--- static/js/posts/fetchposts.js
+++ static/js/posts/fetchposts.js
@@ -1,22 +1,36 @@
 export async function fetchPosts() {
-    try {
-      const response = await fetch("/api/posts", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include", // to include cookies for auth
-      });
-  
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-  
-      const data = await response.json();
-      return data.posts;
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-      throw error;
+  try {
+    const response = await fetch("/api/posts", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // to include cookies for auth
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+
+    const data = await response.json();
+    return data.posts;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    throw error;
   }
-  
+}
+
+// Fetch post details from API
+export async function fetchPostDetails(postId) {
+  const response = await fetch(`/api/post/details?postID=${postId}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+}
