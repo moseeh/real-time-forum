e8c774644736559a80519800863ff072ab87785b
aosindo <andyovvo8@gmail.com>
2025-02-21 12:50:40 +0300

    chore: handle image upload

--- backend/handlers/postshandlers.go
+++ backend/handlers/postshandlers.go
@@ -2,10 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
-	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"real-time-forum/backend/models"
 	"real-time-forum/backend/utils"
@@ -20,11 +21,13 @@ func (h *Handler) CreatePost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	user_id, ok := r.Context().Value(UserIDKey).(string)
 	if !ok {
+		fmt.Println("her1")
 		SendJSONError(w, http.StatusInternalServerError, "User ID not found in context")
 		return
 	}
-	r.Body = http.MaxBytesReader(w, r.Body, 25<<20)
+	// r.Body = http.MaxBytesReader(w, r.Body, 25<<20)
 	if err := r.ParseMultipartForm(20 << 20); err != nil {
+		fmt.Println("her2")
 		http.Error(w, "Error parsing form data: "+err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -34,35 +37,57 @@ func (h *Handler) CreatePost(w http.ResponseWriter, r *http.Request) {
 	categories := r.Form["categories[]"]
 
 	if title == "" || content == "" {
+		fmt.Println("her3")
 		http.Error(w, "Title and content are required", http.StatusBadRequest)
 		return
 	}
+	postID := utils.UUIDGen()
 	var imagePath string
 	file, fileHeader, err := r.FormFile("image")
-	if err != nil {
-		if !errors.Is(err, http.ErrMissingFile) {
-			http.Error(w, "Error retrieving the file: "+err.Error(), http.StatusBadRequest)
-			return
-		}
-	} else {
+	if err != nil && err != http.ErrMissingFile {
+		fmt.Println("her4")
+		http.Error(w, "Error retrieving the file: "+err.Error(), http.StatusBadRequest)
+		return
+
+	}
+
+	if file != nil {
 		defer file.Close()
-		imagePath = "./uploads/" + fileHeader.Filename
 
-		dst, err := os.Create(imagePath)
+		postDir := "static/images"
+
+		// add file extension
+		ext := filepath.Ext(fileHeader.Filename)
+		newFilename := postID + ext
+
+		// difine file path
+		filepath := filepath.Join(postDir, newFilename)
+
+		// Create the directory if it doesn't exist
+		if _, err := os.Stat(postDir); os.IsNotExist(err) {
+			os.Mkdir(postDir, os.ModePerm)
+		}
+
+		// create the file
+		out, err := os.Create(filepath)
 		if err != nil {
-			http.Error(w, "Error saving file: "+err.Error(), http.StatusInternalServerError)
+			fmt.Println("her5")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		defer dst.Close()
 
-		if _, err := io.Copy(dst, file); err != nil {
-			http.Error(w, "Error saving file: "+err.Error(), http.StatusInternalServerError)
+		defer out.Close()
+		if _, err := io.Copy(out, file); err != nil {
+			fmt.Println("her6")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+
+		imagePath = newFilename
 	}
 
 	post := &models.Content{
-		ID:          utils.UUIDGen(),
+		ID:          postID,
 		Title:       title,
 		Text:        content,
 		ImageUrl:    imagePath,
@@ -73,6 +98,7 @@ func (h *Handler) CreatePost(w http.ResponseWriter, r *http.Request) {
 	// Start database transaction
 	tx, err := h.Users.DB.Begin()
 	if err != nil {
+		
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -80,6 +106,7 @@ func (h *Handler) CreatePost(w http.ResponseWriter, r *http.Request) {
 
 	err = h.Users.InsertContent(tx, post)
 	if err != nil {
+		fmt.Println("her5")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
