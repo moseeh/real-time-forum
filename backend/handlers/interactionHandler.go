d266fe4ac126b3c5d9569c053e09a6b3064995ed
moonyango <mosesadrian825@gmail.com>
2025-02-24 05:36:19 +0300

    feat: broadcast to all other users new changes in interactions

--- backend/handlers/interactionHandler.go
+++ backend/handlers/interactionHandler.go
@@ -14,10 +14,11 @@ type InteractionRequest struct {
 }
 
 type InteractionResponse struct {
-	LikesCount    int  `json:"likes_count"`
-	DislikesCount int  `json:"dislikes_count"`
-	IsLiked       bool `json:"is_liked"`
-	IsDisliked    bool `json:"is_disliked"`
+	ContentID     string `json:"content_id"`
+	LikesCount    int    `json:"likes_count"`
+	DislikesCount int    `json:"dislikes_count"`
+	IsLiked       bool   `json:"is_liked"`
+	IsDisliked    bool   `json:"is_disliked"`
 }
 
 func (h *Handler) HandleInteraction(w http.ResponseWriter, r *http.Request) {
@@ -48,6 +49,7 @@ func (h *Handler) HandleInteraction(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	response := InteractionResponse{
+		ContentID:     req.ContentID,
 		LikesCount:    post.LikesCount,
 		DislikesCount: post.DislikesCount,
 		IsLiked:       post.IsLiked,
@@ -56,4 +58,16 @@ func (h *Handler) HandleInteraction(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
+
+	message := Message{
+		Type:       "interaction",
+		SenderID:   user_id,
+		Post:       response,
+	}
+
+	for userid, conn := range users {
+		if userid != user_id{
+			conn.WriteJSON(message)
+		}
+	}
 }
